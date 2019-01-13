05ccba435061908e26d598e1052f279e52f9cdf2
Entreri <tphakasoum@gmail.com>
2019-01-13 09:49:47 -0800

    Updates

---
+++ precache-manifest.514d3acd5e4d30692568d3282c1f3017.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "92ffc2ad21c405f816a4",
+    "url": "/static/js/main.92ffc2ad.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "92ffc2ad21c405f816a4",
+    "url": "/static/css/main.287598fd.chunk.css"
+  },
+  {
+    "revision": "fd0550dacc25783f990ca258508a3714",
+    "url": "/index.html"
+  }
+];
